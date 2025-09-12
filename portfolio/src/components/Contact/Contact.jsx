f7a76d74afd51194d265c66df8734dc7a881b026
TharushiSamindara <tharushisamindara@gmail.com>
2025-09-12 23:43:16 +0530

    Add contact section

---
+++ portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+    return (
+        <footer id="contact" className={styles.container}>
+            <div className={styles.text}>
+                <h2>Contact</h2>
+                <p>Feel free to reach out!</p>
+            </div>
+            <ul className={styles.links}>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+                    <a href="mailto:tharushisamindara@gmail.com">tharushisamindara@gmail.com</a>
+                </li>
+                <li className={styles.link}>
+                    <img
+                        src={getImageUrl("contact/linkedinIcon.png")}
+                        alt="LinkedIn icon"
+                    />
+                    <a href="https://www.linkedin.com/in/tharushi-samindara-250bb9222/">linkedin.com/tharushi-samindara</a>
+                </li>
+                <li className={styles.link}>
+                    <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+                    <a href="https://github.com/TharushiSamindara">github.com/TharushiSamindara</a>
+                </li>
+            </ul>
+        </footer>
+    );
+};
