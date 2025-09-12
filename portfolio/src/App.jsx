45a30dead0a2e6bcd7c69cff439f8023187a3b0d
TharushiSamindara <tharushisamindara@gmail.com>
2025-09-12 12:29:53 +0530

    Add navigation bar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,14 @@
 
 import styles from'./App.module.css'
+import {Navbar} from "./components/Navbar/Navbar.jsx";
+import {Hero} from "./components/Hero/Hero.jsx";
 
 function App() {
 
   return (
     <div className={styles.App}>
-      Hello word!
+      <Navbar />
+      <Hero />
     </div>
   )
 }
